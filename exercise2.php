3870787b07c947227c493ed11bc229d84fc17e99
Alex M - laptop <vootehdoo@gmail.com>
2024-01-08 15:45:34 +0100

    Add exercise 2

---
+++ exercise2.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+
+class Beverage
+{
+  public $color;
+  public $price;
+  public $temperature;
+
+  public function __construct(string $color, float $price, string $temperature = 'cold')
+  {
+    $this->color = $color;
+    $this->price = $price;
+    $this->temperature = $temperature;
+  }
+
+  public function getInfo()
+  {
+    return "This beverage is $this->temperature and $this->color";
+  }  
+}
+
+$cola = new Beverage("black",2);
+
+
+// print_r($cola->getInfo());
+
+/*
+TODO: Make class beer that extends from Beverage.
+TODO: Create the properties name (string) and alcoholPercentage (float).
+TODO: Foresee a construct that's allows us to use all the properties from beverage and that sets the values for name and alcoholpercentage.
+
+Remember for now we will use properties and methods that can be accessed from everywhere.
+TODO: Make a getAlcoholPercentage function which returns the alocoholPercentage.
+TODO: Instantiate an object which represents Duvel. Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
+TODO: Also the name equal to Duvel and the alcohol percentage to 8,5%
+TODO: Print the getAlcoholPercentage 2 times on the screen in two different ways, print the color on the screen and the getInfo.
+
+Make sure that each print is on a different line.
+Try to get this error on the screen= Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholPercentage() in /var/www/becode/workshop/exercise2.php on line 64
+USE TYPEHINTING EVERYWHERE!
+*/
+class Beer extends Beverage
+{
+  public $name;
+  public $alcoholPercentage;
+
+  public function __construct(string $color, float $price, string $temperature = 'cold', string $name, float $alcoholPercentage)
+  {
+    $this->color = $color;
+    $this->price = $price;
+    $this->temperature = $temperature;
+    $this->name = $name;
+    $this->alcoholPercentage = $alcoholPercentage;
+  }
+
+  public function getAlcoholPercentage() 
+  {
+    return $this->alcoholPercentage;
+  }
+}
+
+$duvel = new Beer("blonde", 3.5, "", "Duvel", 8.5);
+
+print_r($duvel->getAlcoholPercentage());
+echo "<br>";
+print_r($duvel->alcoholPercentage);
+
+$cola = new Beverage("black", 2);
+print_r($cola->getAlcoholPercentage());
+
